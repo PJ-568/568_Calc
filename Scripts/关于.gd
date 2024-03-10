9d8d7bf208ba1b5147e78e5f4b49d9792822febc
Lyf986 <3135219449@qq.com>
2024-03-10 21:55:01 +0800

    添加关闭按钮

--- Scripts/关于.gd
+++ Scripts/关于.gd
@@ -5,14 +5,14 @@ extends Control
 
 
 #func _ready():
-	#本场景.set("theme", "res://Assets/样式/得意黑字体.tres")
+    #本场景.set("theme", "res://Assets/样式/得意黑字体.tres")
 
 
 @warning_ignore("unused_parameter")
 func _unhandled_input(event) -> void:
-	if Input.is_action_pressed("归零") or Input.is_action_pressed("ui_menu") or Input.is_action_pressed("ui_home") or Input.is_action_pressed("ui_text_backspace") or Input.is_action_pressed("关于") or Input.is_action_pressed("ui_cancel"):
-		_on_返回_pressed()
+    if Input.is_action_pressed("归零") or Input.is_action_pressed("ui_menu") or Input.is_action_pressed("ui_home") or Input.is_action_pressed("ui_text_backspace") or Input.is_action_pressed("关于") or Input.is_action_pressed("ui_cancel"):
+        _on_返回_pressed()
 
 
 func _on_返回_pressed():
-	get_tree ().change_scene_to_file("res://主界面.tscn")
+    get_tree ().change_scene_to_file("res://主界面.tscn")
